946997914188ca64a010499650d46a96ff136550
James Deane <git@jwdeane.com>
2024-09-16 11:17:51 +1000

    add test site key

--- functions/handler.js
+++ functions/handler.js
@@ -1,17 +1,11 @@
-// This is the demo secret key. In prod, we recommend you store
-// your secret key(s) safely.
-
-const SECRET_KEY = "1x0000000000000000000000000000000AA";
-
 export async function onRequestPost(context) {
-  // return new Response(`Hello!! ${SECRET_KEY}`);
   const body = await context.request.formData();
   const token = body.get("cf-turnstile-response");
   const ip = context.request.headers.get("CF-Connecting-IP");
 
   // Validate the token by calling the "/siteverify" API.
   let formData = new FormData();
-  formData.append("secret", SECRET_KEY);
+  formData.append("secret", context.env.SECRET_KEY);
   formData.append("response", token);
   formData.append("remoteip", ip);
 
@@ -22,8 +16,10 @@ export async function onRequestPost(context) {
       method: "POST",
     }
   );
+
   const outcome = await result.json();
   console.log(outcome);
+
   if (!outcome.success) {
     return new Response(
       "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
